205fc9c25233a5c28643213602f7dfb4a90060f1
damianoimola <damiano.imola@outlook.it>
2021-01-15 23:58:58 +0100

    some fix to the algorithms, cleaning and formatting the code

--- recursion_excercise.py
+++ recursion_excercise.py
@@ -13,15 +13,17 @@ def delete_vowels(s):
             return delete_vowels(s[1:])
         else:
             return s[0] + delete_vowels(s[1:])
-print("# GENERICO 1 #")
-print(delete_vowels("pippo"))
-print(delete_vowels("pipp"))
-print(delete_vowels("pippoo"))
-print(delete_vowels("pippo0"))
-print(delete_vowels("pippo?"))
-print(delete_vowels("1234"))
-print(delete_vowels("aeiou"))
-print(delete_vowels("prova"))
+
+def generic1()
+    print("# GENERIC 1 #")
+    print(delete_vowels("pippo"))
+    print(delete_vowels("pipp"))
+    print(delete_vowels("pippoo"))
+    print(delete_vowels("pippo0"))
+    print(delete_vowels("pippo?"))
+    print(delete_vowels("1234"))
+    print(delete_vowels("aeiou"))
+    print(delete_vowels("prova"))
 
 
 
@@ -50,9 +52,11 @@ def count_digit(L):
             return 1 + count_digit(L[1:])
         else:
             return 0 + count_digit(L[1:])
-print("# GENERICO 2 #")
-print(return_digit("1A2b3C4d"))
-print(count_digit("1A2b3C4d"))
+
+def generic2():
+    print("# GENERIC 2 #")
+    print(return_digit("1A2b3C4d"))
+    print(count_digit("1A2b3C4d"))
 
 
 
@@ -80,8 +84,9 @@ def recursion1a(L):
                 partial += recursion1a([elem])
             return partial + recursion1a(L[1:])
 
-print("\n# PROBLEMA 1a #")
-print(recursion1a([1,5,[2,[1,1],4]]))
+def problem1a():
+    print("\n# PROBLEM 1a #")
+    print(recursion1a([1,5,[2,[1,1],4]]))
 
 
 
@@ -107,8 +112,9 @@ def recursion1b(L):
                     maxVal = actualMax
             return max(maxVal, recursion1b(L[1:]))
 
-print("\n# PROBLEMA 1b #")
-print(recursion1b([1,5,[2,[1,1],4]]))
+def problem1b():
+    print("\n# PROBLEM 1b #")
+    print(recursion1b([1,5,[2,[1,1],4]]))
 
 
 
@@ -128,12 +134,13 @@ def recursion2(a, b):
     else:
         return a * recursion2(a, b-1)
 
-print("\n# PROBLEMA 2 #")
-print(recursion2(2,4))
-print(recursion2(2,5))
-print(recursion2(2,6))
-print(recursion2(10,10))
-print(recursion2(983864,0))
+def problem2():
+    print("\n# PROBLEM 2 #")
+    print(recursion2(2,4))
+    print(recursion2(2,5))
+    print(recursion2(2,6))
+    print(recursion2(10,10))
+    print(recursion2(983864,0))
 
 
 
@@ -150,11 +157,12 @@ def recursion3(S):
     else:
         return S[-1] + recursion3(S[:-1])
 
-print("\n# PROBLEMA 3 #")
-print(recursion3("pippo"))
-print(recursion3("a"))
-print(recursion3(""))
-print(recursion3("prova"))
+def problem3():
+    print("\n# PROBLEMA 3 #")
+    print(recursion3("pippo"))
+    print(recursion3("a"))
+    print(recursion3(""))
+    print(recursion3("prova"))
 
 
 
@@ -162,13 +170,10 @@ print(recursion3("prova"))
 
 
 """ PROBLEMA 4
-Scrivere una funzione ricorsiva in Python che, dato un intero positivo n, ritorna i coefficienti della riga n-esima del triangolo di Tartaglia. Ad esempio, per n= 4, ritorna [1, 3, 3, 1].
+Scrivere una funzione ricorsiva in Python che, data un intero
+positivo n, ritorna i coefficienti della riga n-esima del triangolo di Tartaglia. Ad
+esempio, per n= 4, ritorna [1, 3, 3, 1].
 """
-def recursion4(n):
-# @param n : Int
-# @return Int
-    print()
-
 def recursion4bis(n, i=0):
 # @param n : Int
 # @return list
@@ -194,19 +199,33 @@ def factor(n):
     else:
         return n * factor(n-1)
 
-print("\n# PROBLEMA 4 #")
-print(recursion4bis(1))
-print(recursion4bis(2))
-print(recursion4bis(3))
-print(recursion4bis(4))
-print(recursion4bis(5))
-print(recursion4bis(6))
+def problem4():
+    print("\n# PROBLEM 4 #")
+    print(recursion4bis(1))
+    print(recursion4bis(2))
+    print(recursion4bis(3))
+    print(recursion4bis(4))
+    print(recursion4bis(5))
+    print(recursion4bis(6))
 
 
 
 
 
 """ PROBLEMA 5
+Definire in Python una classe Iterator che prende come
+parametro del costruttore una lista qualsiasi. L’Iterator permette di
+attraversare la lista in entrambe le direzioni, aggiungere e rimuovere un
+elemento subito dopo la posizione in cui si trova lungo la lista e inoltre tiene
+traccia della posizione attuale nella lista. I metodi devono essere:
+1. hasNext: ritorna vero se esiste un elemento dopo la posizione attuale, falso
+altrimenti
+2. hasPrevious: ritorna vero se esiste un elemento prima della posizione
+attuale, falso altrimenti
+3. next: ritorna l’elemento successivo alla posizione attuale
+4. previous: ritorna l’elemento precedente alla posizione attuale
+5. add: aggiunge un elemento subito dopo la posizione attuale
+6. remove: rimuove l’elemento subito dopo la posizione attuale e lo ritorna
 """
 class Iterator:
     def __init__(self, l):
@@ -247,8 +266,7 @@ class Iterator:
     def display(self):
         print(self.list)
 
-
-def excercise5():
+def exercise5():
     print("# PROBLEMA 5 #")
     iterator = Iterator([1,2,3,4,5,6,7,8])
     print(iterator.hasNext())
@@ -280,7 +298,6 @@ def excercise5():
     iterator.display()
     iterator.remove()
     iterator.display()
-excercise5()
 
     
 
@@ -308,8 +325,6 @@ def problem6(P, avg):
             return True
     return False
 
-pict = makeEmptyPicture(50, 50, white)
-print(problem6(pict, 255))
 
 
 
@@ -326,4 +341,123 @@ rimanente, il numero di elementi al suo interno, l’elemento con occupazione
 massima e quello con occupazione minima.
 
 """
-# TODO
+class Item:
+    def __init__(self, name, occupation_units):
+    # @param name : Str
+    # @param place : Int
+        self.name = name
+        self.occupation_units = occupation_units
+
+class Knapsack:
+    def __init__(self, C):
+    # @param C : Int
+        self.capacity = C
+        self.items = []
+
+    def add(self, nome, spazio_occupato):
+    # @param nome : Str
+    # @param spazio_occupato : Int
+        if (self.number_of_items() + spazio_occupato) > self.capacity:
+            print("L'oggetto non entra all'interno dello zaino")
+            return
+        item = Item(nome, spazio_occupato)
+        self.items.append(item)
+        print("Hai aggiunto", nome, "nello zaino")
+
+    def capacity_remaining(self):
+    # @return Int
+        capacity_remaining = self.capacity
+        for elem in self.items:
+            capacity_remaining = capacity_remaining - elem.occupation_units
+        return capacity_remaining
+
+    def number_of_items(self):
+    # @return Int
+        return len(self.items)
+
+    def min_occupation_item_1(self):
+    # @return Item
+        if self.number_of_items()==0:
+            print("Nessn oggetto presente nella lista")
+        else:
+            min_index = 0
+            for counter in range(0, self.number_of_items()):
+                if self.items[counter].occupation_units < self.items[min_index].occupation_units:
+                    min_index = counter
+            return self.items[min_index].name
+
+    def min_occupation_item_2(self):
+    # @return Item
+        if self.number_of_items()==0:
+            print("Nessn oggetto presente nella lista")
+        else:
+            elements = []
+            for elem in self.items:
+                if len(elements) == 0:
+                    elements.append(elem)
+                elif elem.occupation_units < elements[0].occupation_units:
+                    elements.insert(0, elem)
+            return elements[0].name
+
+
+    def min_occupation_item_3(self):
+    # @return Item
+        if self.number_of_items()==0:
+            print("Nessn oggetto presente nella lista")
+        else:
+            elements = []
+            for counter in range(0, self.number_of_items()):
+                item_tuple = (self.items[counter].occupation_units, counter)
+                elements.append(item_tuple)
+            capacity_tuple = min(elements)
+            return self.items[capacity_tuple[1]].name
+
+def problem7():
+    print("### PROBLEM 7 ###")
+    knapsack = Knapsack(10)
+    print("Rimangono", knapsack.capacity_remaining(), "slot liberi")
+    print("Sono presenti", knapsack.number_of_items(), "oggetti")
+
+    knapsack.add("barretta di cioccolato", 1)
+    knapsack.add("astuccio", 2)
+    knapsack.add("portatile", 5)
+    knapsack.add("tavolino", 9)
+    print("Rimangono", knapsack.capacity_remaining(), "slot liberi")
+    print("Sono presenti", knapsack.number_of_items(), "oggetti")
+    print(knapsack.min_occupation_item_1())
+    print(knapsack.min_occupation_item_2())
+    print(knapsack.min_occupation_item_3())
+
+    knapsack.add("penna", 1)
+    print(knapsack.min_occupation_item_1())
+    print(knapsack.min_occupation_item_2())
+    print(knapsack.min_occupation_item_3())
+
+
+
+
+
+
+
+
+# --
+generic1()
+# --
+generic2()
+# --
+problem1a()
+# --
+problem1b()
+# --
+problem2()
+# --
+problem3()
+# --
+problem4()
+# --
+exercise5()
+# --
+pict = makeEmptyPicture(50, 50, white)
+print(problem6(pict, 255))
+# --
+problem7()
